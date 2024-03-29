bbfe77b04ca277321dc1cfcd89e56541098f11ae
RatanakANB <rattanakphok9@gmail.com>
2024-03-29 15:21:49 +0700

    MainCodeBase

---
+++ main.py
@@ -0,0 +1,20 @@
+def add(num1, num2):
+    pass
+
+def subtract(num1, num2):
+    pass
+
+def multiply(num1, num2):
+    pass
+
+def divide(num1, num2):
+    pass
+
+
+
+result1 = add(10, 5)
+result2 = subtract(20, 7)
+result3 = multiply(3, 6)
+result4 = divide(15, 3)
+
+print(result1, result2, result3, result4)
