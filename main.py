3149e7ded080b59b050a40142016b43ce71fbd91
chounan <tch23125@gmail.com>
2024-03-29 16:12:18 +0700

    feature/issue5: make sure the argument is int

--- main.py
+++ main.py
@@ -2,20 +2,25 @@ def add(num1, num2):
     pass
 
 def subtract(num1, num2):
-    return num1 - num2
+    if type(num1) == int and type(num2) == int:
+        return num1 - num2
+    else:
+        return "Please enter only int"
 
 def multiply(num1, num2):
-    return num1 * num2
+    if type(num1) == int and type(num2) == int:
+        return num1 * num2
+    else:
+        return "Please enter only int"
 
 def divide(num1, num2):
     pass
 
 
-try:
-    result1 = add(10, b)
-    result2 = subtract(20, 7)
-    result3 = multiply(3, 6)
-    result4 = divide(15, 3)
-    print(result1, result2, result3, result4)
-except(NameError):
-    print("Please enter only int")
+
+result1 = add(10, 5)
+result2 = subtract(20, 7)
+result3 = multiply(3, "b")
+result4 = divide(15, 3)
+
+print(result1, result2, result3, result4)
